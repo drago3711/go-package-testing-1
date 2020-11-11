ab738679cff420f4040d1c593567a746e1a84558
kbrewer <karledwardbrewer@gmail.com>
2020-11-11 13:42:38 -0500

    Fourth commit

--- cmd/cli1/main.go
+++ cmd/cli1/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/drago3711/go-package-testing-1/pkg/foo"
 	"github.com/drago3711/go-package-testing-1/pkg/notbar"
 )
 
 func main() {
 	fmt.Println("Starting 1")
-	foo.Print()
 	notbar.Print()
 }
