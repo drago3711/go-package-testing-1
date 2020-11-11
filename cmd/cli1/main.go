4a537b2e4acc3c7004e124c1358689fdf5b2d689
kbrewer <karledwardbrewer@gmail.com>
2020-11-11 13:15:55 -0500

    Second commit

--- cmd/cli1/main.go
+++ cmd/cli1/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/drago3711/go-package-testing-1/pkg/foo"
+	"github.com/drago3711/go-package-testing-1/pkg/notbar"
 )
 
 func main() {
 	fmt.Println("Starting 1")
 	foo.Print()
+	notbar.Print()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/drago3711/go-package-testing-1
 
 go 1.15
+
+require github.com/drago3711/go-package-testing-2 v0.0.2
